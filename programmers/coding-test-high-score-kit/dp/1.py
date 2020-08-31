146519660dbc8c50e1fab2baf5d88b9ac10313b0
PSM <zzererer@naver.com>
2020-08-31 17:10:54 +0900

    200831 commit - 2 dp/1.py
    - dp 1번문제 상원님 코드 기록
    - 한 번 해결해볼 필요 있음

--- programmers/coding-test-high-score-kit/dp/1.py
+++ programmers/coding-test-high-score-kit/dp/1.py
@@ -20,3 +20,48 @@ def solution(N, number):
 
 a = solution(5, 12)
 print(a)
+
+# 상원님
+# def func(y, N):
+#     y = str(y)
+#     N = str(N)
+#     for x in y:
+#         if x != N: return False
+#     return True
+#
+#
+# def solution(N, number):
+#     answer = 0
+#
+#     cont = []
+#     part = []
+#     check = set()
+#
+#     cont.append([0, N])
+#
+#     for idx in range(1, 9):
+#
+#         for x, y in cont:
+#
+#             if x == number: return idx
+#             if (x, y) in check: continue
+#             check.add((x, y))
+#
+#             # 요기
+#             cont.append([x + y, 0])
+#             cont.append([x - y, 0])
+#             cont.append([x * y, 0])
+#             if (y != 0): cont.append([x // y, 0])
+#         # 요기
+#
+#         part.append([x, y + N])
+#         part.append([x, y - N])
+#         part.append([x, y * N])
+#         part.append([x, y // N])
+#
+#         if func(y, N): part.append([x, y * 10 + N])
+#
+#     cont = part[:]
+#
+#
+# return -1
