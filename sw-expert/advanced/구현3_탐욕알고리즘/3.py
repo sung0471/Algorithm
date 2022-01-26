e5a06a36a03113926b8197c006b94a661b3656d3
sung0471 <zzererer@naver.com>
2022-01-27 00:37:32 +0900

    22.1.27 - 1 구현3_탐욕알고리즘 1문제
    ✔ 해결한 문제
    
    ✔ 미해결 문제
    3일차 - 베이비진 게임

---
+++ sw-expert/advanced/구현3_탐욕알고리즘/3.py
@@ -0,0 +1,46 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    card_l = list(map(int, input().split()))
+    card_num = []
+    for _ in range(2):
+        card_num.append([0 for i in range(10)])
+    result = -1
+
+    # print('{} : {}, {}'.format(test_case, card_l[::2], card_l[1::2]))
+
+    for i, num in enumerate(card_l):
+        player = 0 if i % 2 == 0 else 1
+
+        # 하나의 숫자가 3번나오면 끝내기
+        if num not in card_num:
+            card_num[player][num] = 1
+        else:
+            card_num[player][num] += 1
+            if card_num[player][num] == 3:
+                result = player
+                break
+
+        # 연속된 숫자가 3개 이상인지 판단하기
+        if num == 0 and card_num[player][num] and card_num[player][num + 1] and card_num[player][num + 2]:
+            result = player
+            break
+        if 0 < num < 9 and card_num[player][num - 1] and card_num[player][num] and card_num[player][num + 1]:
+            result = player
+            break
+        if 1 < num < 8:
+            if card_num[player][num - 2] and card_num[player][num - 1] and card_num[player][num]:
+                result = player
+                break
+            elif card_num[player][num - 1] and card_num[player][num] and card_num[player][num + 1]:
+                result = player
+                break
+            elif card_num[player][num] and card_num[player][num + 1] and card_num[player][num + 2]:
+                result = player
+                break
+        if num == 9 and card_num[player][num - 2] and card_num[player][num - 1] and card_num[player][num]:
+            result = player
+            break
+
+    print('#{} {}'.format(test_case, result + 1))
+
